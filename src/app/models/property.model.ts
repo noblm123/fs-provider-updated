8254d70504a452b0765a27f748b789697419c610
Mark Noble <marknoble@Marks-MBP.lan>
2019-05-30 09:49:13 +0100

    commit

---
+++ src/app/models/property.model.ts
@@ -0,0 +1,7 @@
+export class Property {
+    name: string;
+
+    constructor() {
+        this.name = "";
+    }
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Property } from '../models'
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  properties: Array<Property> = [];
+
+  constructor() {
+
+    let property1 = new Property();
+    property1.name = "One";
+
+    let property2 = new Property();
+    property2.name = "Two";
+
+    this.properties.push(property1);
+    this.properties.push(property2);
+  }
 
 }
